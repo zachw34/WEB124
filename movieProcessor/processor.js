172ed5c96744fd574ab767353586df84292f49dd
zachw34 <zachw3434@gmail.com>
2026-06-28 16:02:04 -0500

    Update processor.js

--- movieProcessor/processor.js
+++ movieProcessor/processor.js
@@ -1,1 +1,24 @@
+// Assignment 3 Zach Willis, June 28, 2026 //
 
+const rawMovieData = [
+  "Ready Player One,sci-fi,7.4,oasisRP1@email.com",
+  "Obsession,horror,8.1,obsessed@email.com",
+  "The Godfather,drama,9.2,corleone@mob.com",
+  "Toy Story,animation,8.3,woody@pixar.com",
+  "Interstellar,sci-fi,8.6",
+];
+
+const movieObjects = [];
+
+function Movie(title, genre, rating, reviewEmail) {
+  this.title = title;
+  this.genre = genre;
+  this.rating = parseFloat(rating); // Converts the string rating to a number
+  this.reviewEmail = reviewEmail || "N/A"; // Handles missing email
+  this.id = Symbol('uniqueMovieId'); // Unique identifier
+}
+
+for (const movieString of movieData) {
+  // Split the string into individual pieces by the coma
+  const [title, genre, rating, reviewEmail] = movieString.split(',');
+  
