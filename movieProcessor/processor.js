79d984ed7c10a77f1ba67d468cb93fa7874657f4
zachw34 <zachw3434@gmail.com>
2026-06-28 17:39:44 -0500

    Update processor.js

--- movieProcessor/processor.js
+++ movieProcessor/processor.js
@@ -1,5 +1,6 @@
 // Assignment 3 Zach Willis, June 28, 2026 //
 
+// Part 1 Raw Data
 const rawMovieData = [
   "Ready Player One,sci-fi,7.4,oasisRP1@email.com",
   "Obsession,horror,8.1,obsessed@email.com",
@@ -8,6 +9,7 @@ const rawMovieData = [
   "Interstellar,sci-fi,8.6"
 ];
 
+// Part 2 Create Movie Objects
 const movieObjects = [];
 
 function Movie(title, genre, rating, reviewEmail) {
@@ -27,3 +29,29 @@ const newMovie = new Movie(title, genre, rating, reviewEmail);
   movieObjects.push(newMovie);
 }
 console.log(movieObjects);
+
+// Part 3 Add Methods
+// 1. Add methods
+Movie.prototype.getSummary = function() {
+  return `${this.title} is a ${this.genre} movie with a rating of ${this.rating}`;
+};
+
+Movie.prototype.isHighlyRated = function() {
+  return this.rating >= 8;
+};
+
+Movie.prototype.getReviewEmail = function() {
+  // 2. Returns the email using optional chaining (safely evaluates to undefined if missing), or defaults to 'none'
+  return this.reviewEmail?.toLowerCase() || 'none'; 
+};
+
+Movie.prototype.getID = function() {
+  return this.id;
+};
+
+// Part 4 Array and String Processing
+
+// Testing the output
+console.log(movieObjects);
+console.log(movieObjects[0].getSummary());
+console.log(movieObjects[0].getReviewEmail());
