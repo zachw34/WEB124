abaf0f818eeb1441ad8f610fbc998332129eaf49
zachw34 <zachw3434@gmail.com>
2026-07-03 12:30:42 -0500

    Update explorer.js

--- bookExplorer/explorer.js
+++ bookExplorer/explorer.js
@@ -1,5 +1,11 @@
 // Assignment 4 Zach Willis, July 2, 2026 //
 
 // Part 1 Book Data
-
-
+// Quotations for 
+const books = [
+  { title: "Dune", author: "Frank Herbert", pages: 412 },
+  { title: "Project Hail Mary", author: "Andy Weir", pages: 476 },
+  { title: "The Odyssey", author: "Homer", pages: 384 },
+  { title: "To Kill a Mockingbird", author: "Harper Lee", pages: 281 },
+  { title: "The Score", author: "Ellie Kennedy", pages: 394 }
+];
