07d67c7cbfb6e3ea141346b574c37dc72251ae2c
zachw34 <zachw3434@gmail.com>
2026-07-03 16:33:05 -0500

    Update explorer.js

--- bookExplorer/explorer.js
+++ bookExplorer/explorer.js
@@ -28,5 +28,17 @@ console.log(document.body.firstElementChild);
 console.log(document.body.children);
 
 // Part 4 Walking the DOM
+
 // Getting the ul element
 console.log(document.body.children)[2];
+// Getting the parent of that li
+console.log(document.querySelector('li').parentElement);
+// Getting sibling of the initially selected li
+console.log(document.querySelectorAll('li')[0].nextElementSibling);
+
+// Part 5 Node Properties
+
+// Getting text from inside first li
+console.log(document.getElementsByTagName('li')[0]);
+// Display type of node - confirmation of element: 1
+console.log(document.querySelectorAll('li')[0].nodeType);
