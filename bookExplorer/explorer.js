26782c5846704431e3f597b27b5f9acccd129a1f
zachw34 <zachw3434@gmail.com>
2026-07-03 12:55:09 -0500

    Update explorer.js

--- bookExplorer/explorer.js
+++ bookExplorer/explorer.js
@@ -1,11 +1,17 @@
 // Assignment 4 Zach Willis, July 2, 2026 //
 
 // Part 1 Book Data
-// Quotations for 
-const books = [
+
+const bookData = [
   { title: "Dune", author: "Frank Herbert", pages: 412 },
   { title: "Project Hail Mary", author: "Andy Weir", pages: 476 },
   { title: "The Odyssey", author: "Homer", pages: 384 },
   { title: "To Kill a Mockingbird", author: "Harper Lee", pages: 281 },
   { title: "The Score", author: "Ellie Kennedy", pages: 394 }
 ];
+
+// Part 2 Console Output everything 
+
+bookData.forEach((book) => {
+  console.log(`Title: ${book.title} | Author: ${book.author} | Pages: ${book.pages}`);
+});
