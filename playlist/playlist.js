59f7e7e00a01462f3af1cde568737cdd0cc6d482
zachw34 <zachw3434@gmail.com>
2026-07-12 18:45:49 -0500

    Update playlist.js

--- playlist/playlist.js
+++ playlist/playlist.js
@@ -2,18 +2,19 @@
 
 // Part 1 Selecting Elements
 
-// Accessing update playlist button
-
+// 1. Accessing update playlist button
 const playlistButton = document.getElementById("update-btn");
 
-// To confirm it works
-
-playlistButton.addEventListener(`click`, () => {
-  console.log("Button works good!");
-});
-
-// querySelector for p tag
-
+// 2. querySelector for p tag
 const myParagraph = document.querySelector("p");
   console.log(myParagraph);
 
+// 3. Console log both 
+console.log(playlistButton, myParagraph);
+
+// Part 2 Browser Events
+
+// 1. To confirm it works
+playlistButton.addEventListener(`click`, () => {
+  console.log("Button works good!");
+});
