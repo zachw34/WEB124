9a8b74e497646328b99dc06ef6fb8e2a6e763c81
zachw34 <zachw3434@gmail.com>
2026-07-12 18:07:11 -0500

    Update playlist.js

--- playlist/playlist.js
+++ playlist/playlist.js
@@ -1,1 +1,13 @@
+// Assignment 5 Zach Willis, July 12, 2026 //
 
+// Part 1 Selecting Elements
+
+// Accessing update playlist button
+
+const playlistButton = document.getElementById("update-btn");
+
+// To confirm it works
+
+playlistButton.addEventListener("Click", () => {
+  console.log("Button works good!");
+});
