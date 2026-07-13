cd3d202b693f901f5a7e2b93195cbe60cec57f74
zachw34 <zachw3434@gmail.com>
2026-07-12 19:22:49 -0500

    Update playlist.js

--- playlist/playlist.js
+++ playlist/playlist.js
@@ -14,9 +14,14 @@ console.log(playlistButton, myParagraph);
 
 // Part 2 Browser Events
 
-// 1. To confirm it works
+// 1. To create click event for button
 playlistButton.addEventListener(`click`, () => {
   const heading = document.querySelector('h1');
   // Change text
   heading.textContent = 'The 2000s Hits Playlist';
+});
+
+// 1. To create click event for p tag
+const introText = document.querySelector("p");
+introText.textContent = "A lot of my favorite oldies but goodies come from the 2000s. Some of the greatest party hits came from this decade and they still get played today!";
 });
