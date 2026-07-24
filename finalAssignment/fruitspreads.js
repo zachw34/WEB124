6a3865420b9db1171217bac54007fe902b133758
zachw34 <zachw3434@gmail.com>
2026-07-23 19:33:10 -0500

    Update fruitspreads.js

--- finalAssignment/fruitspreads.js
+++ finalAssignment/fruitspreads.js
@@ -1,1 +1,15 @@
+// Final Assignment Zach Willis, July 19, 2026 //
 
+// Part 2: JavaScript Objects and Arrays
+
+const fruitSpreads = [
+  {
+    category: "Jams",
+    items: ["Bacon Jam", "Blackberry Jam", "Raspberry Jam", "Strawberry Jam"]
+  },
+  {
+    category: "Jellies",
+    items: ["Apple Jelly", "Grape Jelly", "Peach Jelly", "Prickly Pear Jelly"]
+  }
+  console.log(fruitSpreads);
+  ];
