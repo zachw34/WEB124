34c4d6b9b3bf116839f32f0d8cbca72c09372b2d
zachw34 <zachw3434@gmail.com>
2026-07-27 19:51:46 -0500

    Update fruitspreads.js

--- finalAssignment/fruitspreads.js
+++ finalAssignment/fruitspreads.js
@@ -125,6 +125,11 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
+// Part 8: Storage
+// STORE THE DATA IN LOCALSTORAGE
+    localStorage.setItem('savedUserName', userName);
+    localStorage.setItem('savedUserEmail', userEmail);
+
 // Part 9: Reflection
 // At the bottom of your JavaScript file, answer the following questions as comments:
 
